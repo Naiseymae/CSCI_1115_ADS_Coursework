6df3b53131841f72eef1b3e9e2cbf2fb5acd9655
Renee Linford <48326687+Naiseymae@users.noreply.github.com>
2019-10-24 09:26:27 -0600

    Delete package line.

--- Chapter24/Exercise24_1.java
+++ Chapter24/Exercise24_1.java
@@ -1,4 +1,3 @@
-package exercise_24_1;
 
 /* Author: Renee Linford
  * Date: 10-22-19
@@ -12,38 +11,36 @@ public class Exercise24_1 {
 	  
 		// Two MyArrayLists, list1 and list2, with the initial values.
 		String[] name1 = {"Tom", "George", "Peter", "Jean", "Jane"};
-	    String[] name2 = {"Tom", "George", "Michael", "Michelle", "Daniel"};
+		String[] name2 = {"Tom", "George", "Michael", "Michelle", "Daniel"};
 	    
-	    // Create list1 and list2 with the initial values.
-	    MyList<String> list1 = new MyArrayList<String>(name1);   
-	    MyList<String> list2 = new MyArrayList<String>(name2);   
-	    System.out.println("list1:" + list1);
-	    System.out.println("list2:" + list2);
-	    
-	    // Invoke list1.addAll(list2), and display list1 and list2.
-	    list1.addAll(list2);
-	    System.out.println("After addAll list1:" + list1 + "\n");
-	    
-	    // Recreate list1 and list2 with the same initial values.
-	    list1 = new MyArrayList<String>(name1);
-	    list2 = new MyArrayList<String>(name2);   
-	    System.out.println("list1:" + list1);
-	    System.out.println("list2:" + list2);
-	    
-	    // Invoke list1.removeAll(list2), and display list1 and list2.
-	    list1.removeAll(list2);
-	    System.out.println("After removeAll list1:" + list1 + "\n");
-	    
-	    // Recreate list1 and list2 with the same initial values.
-	    list1 = new MyArrayList<String>(name1);
-	    list2 = new MyArrayList<String>(name2);   
-	    System.out.println("list1:" + list1);
-	    System.out.println("list2:" + list2);
-	    
-	    // Invoke list1.retainAll(list2), and display list1 and list2.
-	    list1.retainAll(list2);
-	    System.out.println("After retainAll list1:" + list1 + "\n");
-		
+		// Create list1 and list2 with the initial values.
+		MyList<String> list1 = new MyArrayList<String>(name1);   
+		MyList<String> list2 = new MyArrayList<String>(name2);   
+		System.out.println("list1:" + list1);
+		System.out.println("list2:" + list2);
+
+		// Invoke list1.addAll(list2), and display list1 and list2.
+		list1.addAll(list2);
+		System.out.println("After addAll list1:" + list1 + "\n");
+
+		// Recreate list1 and list2 with the same initial values.
+		list1 = new MyArrayList<String>(name1);
+		list2 = new MyArrayList<String>(name2);   
+		System.out.println("list1:" + list1);
+		System.out.println("list2:" + list2);
+
+		// Invoke list1.removeAll(list2), and display list1 and list2.
+		list1.removeAll(list2);
+		System.out.println("After removeAll list1:" + list1 + "\n");
+
+		// Recreate list1 and list2 with the same initial values.
+		list1 = new MyArrayList<String>(name1);
+		list2 = new MyArrayList<String>(name2);   
+		System.out.println("list1:" + list1);
+		System.out.println("list2:" + list2);
+
+		// Invoke list1.retainAll(list2), and display list1 and list2.
+		list1.retainAll(list2);
+		System.out.println("After retainAll list1:" + list1 + "\n");
 	}
-	
 }
