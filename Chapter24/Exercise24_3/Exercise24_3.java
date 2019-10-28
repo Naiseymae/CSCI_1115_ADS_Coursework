fe076af44af99cb1c6a1c98de289301d94bcb9ca
Renee Linford <48326687+Naiseymae@users.noreply.github.com>
2019-10-28 10:30:50 -0600

    Add files via upload

---
+++ Chapter24/Exercise24_3/Exercise24_3.java
@@ -0,0 +1,29 @@
+package exercise_24_3;
+
+/* Author: Renee Linford
+ * Date: 10-23-19
+ * ADS Exercise 24-3: Implement a doubly linked list.
+ */
+
+public class Exercise24_3 {
+
+	public static void main(String[] args) {
+		
+		TwoWayLinkedList<Integer> list = new TwoWayLinkedList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+
+		java.util.ListIterator<Integer> iterator = list.iterator();
+		iterator.next();
+		iterator.next();
+		iterator.next();
+
+		// System.out.print(iterator.next() + " ");
+
+		System.out.println();
+		while (iterator.hasPrevious())
+				System.out.print(iterator.previous() + " ");
+	}
+}
